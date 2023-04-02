3cc612e542ac33f0acebe9283ed0b3251ded90b9
Pavel Yosifovich <zodiacon@live.com>
2023-04-02 11:46:32 -0400

    Add project files.

---
+++ PoolMonX/MainFrm.cpp
@@ -0,0 +1,66 @@
+// MainFrm.cpp : implmentation of the CMainFrame class
+//
+/////////////////////////////////////////////////////////////////////////////
+
+#include "pch.h"
+#include "resource.h"
+#include "AboutDlg.h"
+#include "MainFrm.h"
+
+BOOL CMainFrame::PreTranslateMessage(MSG* pMsg) {
+	return CFrameWindowImpl<CMainFrame>::PreTranslateMessage(pMsg);
+}
+
+BOOL CMainFrame::OnIdle() {
+	UIUpdateToolBar();
+	return FALSE;
+}
+
+LRESULT CMainFrame::OnCreate(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& /*bHandled*/) {
+	CreateSimpleStatusBar();
+	UISetCheck(ID_VIEW_STATUS_BAR, 1);
+
+	// register object for message filtering and idle updates
+	auto pLoop = _Module.GetMessageLoop();
+	ATLASSERT(pLoop != nullptr);
+	pLoop->AddMessageFilter(this);
+	pLoop->AddIdleHandler(this);
+
+	return 0;
+}
+
+LRESULT CMainFrame::OnDestroy(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& bHandled) {
+	// unregister message filtering and idle updates
+	CMessageLoop* pLoop = _Module.GetMessageLoop();
+	ATLASSERT(pLoop != nullptr);
+	pLoop->RemoveMessageFilter(this);
+	pLoop->RemoveIdleHandler(this);
+
+	bHandled = FALSE;
+	return 1;
+}
+
+LRESULT CMainFrame::OnFileExit(WORD /*wNotifyCode*/, WORD /*wID*/, HWND /*hWndCtl*/, BOOL& /*bHandled*/) {
+	PostMessage(WM_CLOSE);
+	return 0;
+}
+
+LRESULT CMainFrame::OnFileNew(WORD /*wNotifyCode*/, WORD /*wID*/, HWND /*hWndCtl*/, BOOL& /*bHandled*/) {
+	// TODO: add code to initialize document
+
+	return 0;
+}
+
+LRESULT CMainFrame::OnViewStatusBar(WORD /*wNotifyCode*/, WORD /*wID*/, HWND /*hWndCtl*/, BOOL& /*bHandled*/) {
+	BOOL bVisible = !::IsWindowVisible(m_hWndStatusBar);
+	::ShowWindow(m_hWndStatusBar, bVisible ? SW_SHOWNOACTIVATE : SW_HIDE);
+	UISetCheck(ID_VIEW_STATUS_BAR, bVisible);
+	UpdateLayout();
+	return 0;
+}
+
+LRESULT CMainFrame::OnAppAbout(WORD /*wNotifyCode*/, WORD /*wID*/, HWND /*hWndCtl*/, BOOL& /*bHandled*/) {
+	CAboutDlg dlg;
+	dlg.DoModal();
+	return 0;
+}
